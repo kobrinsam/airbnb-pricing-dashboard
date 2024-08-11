67d41304eea0d71484a7fc85dcc1687373e93ead
sachikou@umich.edu <sachikou@umich.edu>
2024-08-11 00:29:40 -0700

    Update homepage for Streamlit app

--- Home.py
+++ Home.py
@@ -2,10 +2,33 @@ import streamlit as st
 
 st.set_page_config(
     page_title="MADS Capstone Project",
-    page_icon="👋",
+    page_icon="🏠",
 )
 
-st.write("# University of Michigan")
-st.write("Placeholder for homepage")
+st.markdown("""
+    <div style="text-align: center;">
+        <h1>Airbnb Price Prediction</h1>
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="text-align: center;">
+        Unlock your Airbnb potential with pricing strategy. <br> Our new app offers a solution for Airbnb hosts to predict prices based on your property’s characteristics.
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="text-align: center;">
+        <hr style="width: 100%; border-top: 2px solid #ccc;">
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="text-align: center;">
+        SIADS 699 | Team 14: Big House <br> Sam Kobrin, Alex Chan, Sachiko Uchikoshi <br> - 2024 August -
+    </div>
+""", unsafe_allow_html=True)
+
+# st.image("hana.PNG", width=300)
 
 st.sidebar.success("Select a page above.")
