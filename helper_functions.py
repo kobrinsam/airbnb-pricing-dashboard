039be514fe2d8bdba3ebff8685484e9e519dce5d
sachikou@umich.edu <sachikou@umich.edu>
2024-08-10 19:17:07 -0700

    Added function to connect to Snowflake

--- helper_functions.py
+++ helper_functions.py
@@ -5,6 +5,44 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 from snowflake.connector.pandas_tools import write_pandas
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+def connect_to_snowflake(schema_name=None):
+    # Load environment variables from a .env file
+    load_dotenv()
+
+    TEMP_USER = os.getenv('SNOWSQL_TEMP_USER')
+    TEMP_USER_PASSWORD = os.getenv('SNOWSQL_TEMP_PWD')
+    SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
+    
+    if not TEMP_USER:
+        logger.error("Environment variable SNOWSQL_TEMP_USER must be set")
+        return None
+    if not TEMP_USER_PASSWORD:
+        logger.error("Environment variable SNOWSQL_TEMP_PWD must be set")
+        return None
+
+    try:
+        conn = snowflake.connector.connect(
+            user=TEMP_USER,
+            password=TEMP_USER_PASSWORD,
+            account=SNOWFLAKE_ACCOUNT,
+            warehouse='COMPUTE_WH',
+            database='AIRBNB',
+            schema=schema_name
+        )
+
+        print(f'Successfully connected to Snowflake schema {schema_name}')
+        return conn
+
+    except snowflake.connector.errors.Error as e:
+        logger.error(f'Failed to connect to Snowflake due to error: {e}')
+        return None
+
 
 def get_data(sql_query, conn, date_columns=None):
     """
@@ -12,6 +50,8 @@ def get_data(sql_query, conn, date_columns=None):
 
     Args:
         sql_query (str): SQL query to execute.
+        conn (snowflake.connector): Snowflake connection object.
+        date_columns (list, optional): List of columns to parse as dates
 
     Returns:
         df_result: Resulting DataFrame from the SQL query.
@@ -39,31 +79,37 @@ def get_data(sql_query, conn, date_columns=None):
     return df_result
 
 
-def write_to_snowflake(df_name, conn, snowflake_schema_name, snowflake_table_name):
+def write_to_snowflake(df_name, conn, schema_name, table_name, overwrite_table=False):
     """
-    Executes a SQL query to write data to Snowflake.
+    Writes data from dataframe to Snowflake.
 
     Args:
         df_name (str): Name of the DataFrame to write to Snowflake.
+        conn (snowflake.connector): Snowflake connection object.
         snowflake_schema_name (str): Name of the schema to write to in Snowflake
         snowflake_table_name (str): Name of the table to write to in Snowflake.
-        conn (snowflake.connector): Snowflake connection object.
     Returns:
-        None. Writes data to Snowflake.
+        Writes data to Snowflake.
     """
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M%S')
+
+    schema_name = str.upper(schema_name)
+    table_name = str.upper(table_name)
+
     try:
         success, num_chunks, num_rows, output = write_pandas(
             conn=conn,
             df=df_name,
-            schema= str.upper(snowflake_schema_name), # schema needs to be capitalized
-            table_name=snowflake_table_name,
+            schema=schema_name, # schema needs to be capitalized
+            table_name=table_name,
             auto_create_table=True,
-            overwrite=True
+            overwrite=overwrite_table
         )
 
-        print(f'Created table and data loaded to Snowflake at {current_time}')
+        if overwrite_table:
+            print(f'Table {table_name} created in schema {schema_name} at {current_time}')
+        else:
+            print(f'Data appended to table {table_name} in schema {schema_name} at {current_time}')
 
     except Exception as e:
-        print(f'Failed to create table and load data to Snowflake due to error code {e}')
-    return None
+        logger.error(f'Failed to create table and load data to Snowflake due to error code {e}')
