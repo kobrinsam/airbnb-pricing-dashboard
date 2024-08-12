4b235f6ab501f7889ffcbdca7833600f3543050b
sachikou@umich.edu <sachikou@umich.edu>
2024-08-11 20:18:53 -0700

    Update home

--- Home.py
+++ Home.py
@@ -11,12 +11,6 @@ st.image("photo_1.png", use_column_width=True)
 
 st.markdown("""
     <div style="text-align: center;">
-        <h1>Airbnb Price Prediction</h1>
-    </div>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-    <div style="text-align: center;">
         Unlock your Airbnb potential with pricing strategy. <br> Our new app offers a solution for Airbnb hosts to predict prices based on your property’s characteristics.
     </div>
 """, unsafe_allow_html=True)
