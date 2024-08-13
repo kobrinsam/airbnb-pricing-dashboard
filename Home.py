7706acddfb99b604a3d1d2a515f60e8cbac83aab
sachikou@umich.edu <sachikou@umich.edu>
2024-08-12 22:46:36 -0700

    Update markets

--- Home.py
+++ Home.py
@@ -17,18 +17,15 @@ st.markdown("""
 
 st.markdown("""
     <div style="text-align: center;">
-        Markets: Albany, NY; New York City, NY; Chicago, IL; Los Angeles, CA; San Francisco, CA; Seattle, WA; Washington, D.C. 
-    </div>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-    <div style="text-align: center;">
         <hr style="width: 100%; border-top: 2px solid #ccc;">
     </div>
 """, unsafe_allow_html=True)
 
 st.subheader("Motivation")
 st.write("As platforms like Airbnb and VRBO have grown in popularity, hosting has become a common way to make additional income. However, setting the right price for a property can be difficult. Each listing is unique, affected by factors like location, amenities, and the number of bedrooms and bathrooms. Additionally, demand fluctuates with the day of the week, peak seasons, weather conditions, and holidays. Although tools such as AirDNA can help with pricing, they often involve sharing personal information, which can be a privacy concern. We aimed to develop an open-source tool that allows Airbnb hosts to estimate listing prices based on their property's attributes while ensuring privacy is preserved.")
+
+st.subheader("Markets")
+st.write("Albany, NY; New York City, NY; Chicago, IL; Los Angeles, CA; San Francisco, CA; Seattle, WA; Washington, D.C.")
 
 st.subheader("Features")
 st.markdown("- Interactive dashboard with user inputs for listing details.")
