be78c32e223fa69f6b8b867a24ec69d4475dd543
sachikou@umich.edu <sachikou@umich.edu>
2024-08-12 22:25:56 -0700

    add app introduction

--- Home.py
+++ Home.py
@@ -21,6 +21,26 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
+st.subheader("Motivation")
+st.write("As platforms like Airbnb and VRBO have grown in popularity, hosting has become a common way to make additional income. However, setting the right price for a property can be difficult. Each listing is unique, affected by factors like location, amenities, and the number of bedrooms and bathrooms. Additionally, demand fluctuates with the day of the week, peak seasons, weather conditions, and holidays. Although tools such as AirDNA can help with pricing, they often involve sharing personal information, which can be a privacy concern. We aimed to develop an open-source tool that allows Airbnb hosts to estimate listing prices based on their property's attributes while ensuring privacy is preserved.")
+
+st.subheader("Features")
+st.markdown("- Interactive dashboard with user inputs for listing details")
+st.markdown("- Price prediction using a trained machine learning model")
+st.markdown("- Map visualization to select the potential Airbnb listing location")
+
+st.subheader("Hot to use")
+st.markdown("- Go to **Price Prediction** page.  Enter the details of your Airbnb listing.")
+st.markdown("- The dashboard will display the predicted list price for your Airbnb listing.")
+st.markdown("- Leverage the insights provided to strategically set your listing price.")
+
+
+st.markdown("""
+    <div style="text-align: center;">
+        <hr style="width: 100%; border-top: 2px solid #ccc;">
+    </div>
+""", unsafe_allow_html=True)
+
 st.markdown("""
     <div style="text-align: center;">
         SIADS 699 | Team 14: Big House <br> Sam Kobrin, Sachiko Uchikoshi, Alex Chan <br> - 2024 August -
